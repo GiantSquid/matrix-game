b4a25ad82aa5095b9b0022b88664a00abb55824e
Sergey Matiyasevich <FoxRandom@Foxs-MacBook-Pro.local>
2014-12-22 17:51:25 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,30 @@
+// Copyright: FoxLab 2014
+
+// Layout State Vars:
+
+var LSV = {
+	
+	isLandscape : true,
+	scale : 1,
+	rotate : 0,
+	isSetPan : false,
+	isUrlPan : false,
+	isSharePan : false,
+	fxTitle : 'FoxTools: Adaptivity Checker'
+	
+}
+
+// UI Basic Actions:
+
+
+// UI Complex Triggers & Behaviors:
+
+
+// Event Hadlerss:
+
+
+// App Initialisation:
+
+function init() {
+	console.log( 'Application started' );
+}
